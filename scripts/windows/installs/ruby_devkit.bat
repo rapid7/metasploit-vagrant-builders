98d1f3d59a745e0778e7a7fb25caff382b1720dd
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2018-07-09 22:07:04 -0500

    add installer tools

--- scripts/windows/installs/ruby_devkit.bat
+++ scripts/windows/installs/ruby_devkit.bat
@@ -2,6 +2,6 @@ choco install -y ruby2.devkit
 setx PATH "%PATH%;C:\tools\DevKit2\bin"
 cd C:\tools\DevKit2
 echo "- C:\tools\ruby24" >> config.yml
-ruby dk.rb install --force
-ridk install 3
+C:\tools\ruby24\bin\ruby.exe dk.rb install --force
+C:\tools\ruby24\bin\ridk.cmd install 3
 refreshenv
--- scripts/windows/installs/wixtoolset.bat
+++ scripts/windows/installs/wixtoolset.bat
@@ -1,4 +1,4 @@
 chocolatey feature enable -n=allowGlobalConfirmation
-choco install visualstudiocommunity2013
+choco install wixtoolset
 chocolatey feature disable -n=allowGlobalConfirmation
-setx PATH "%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x64"
+setx PATH "%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin"
