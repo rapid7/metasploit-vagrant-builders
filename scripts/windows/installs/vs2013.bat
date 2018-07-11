b316ff489931277ad32d8bbdf0797d955239f776
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2018-07-11 10:24:14 -0500

    power and path configs

--- scripts/windows/installs/vs2013.bat
+++ scripts/windows/installs/vs2013.bat
@@ -1,4 +1,3 @@
 chocolatey feature enable -n=allowGlobalConfirmation
 choco install visualstudiocommunity2013
 chocolatey feature disable -n=allowGlobalConfirmation
-setx PATH "%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x64"
--- scripts/windows/installs/wixtoolset.bat
+++ scripts/windows/installs/wixtoolset.bat
@@ -1,4 +1,3 @@
 chocolatey feature enable -n=allowGlobalConfirmation
 choco install wixtoolset
 chocolatey feature disable -n=allowGlobalConfirmation
-setx PATH "%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin"
