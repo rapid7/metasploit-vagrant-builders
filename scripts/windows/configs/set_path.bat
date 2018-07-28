c4adcf7e328992377aba17797963f54714424cd3
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2018-07-28 11:25:53 -0500

    make path set global

--- scripts/windows/configs/set_path.bat
+++ scripts/windows/configs/set_path.bat
@@ -1,1 +1,1 @@
-setx PATH "%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin;C:\Program Files (x86)\Windows Kits\8.1\bin\x64"
+setx -m PATH "%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin;C:\Program Files (x86)\Windows Kits\8.1\bin\x64"
