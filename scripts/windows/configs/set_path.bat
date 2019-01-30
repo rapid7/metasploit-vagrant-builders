d0b6c5486da81c54c1c25bbfea376b8454964398
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2019-01-30 15:19:58 -0600

    add msys64/usr/bin to path

--- scripts/windows/configs/set_path.bat
+++ scripts/windows/configs/set_path.bat
@@ -1,1 +1,1 @@
-setx -m PATH "%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin;C:\Program Files (x86)\Windows Kits\8.1\bin\x64"
+setx -m PATH "%PATH%;C:\PROGRA~2\WIXTOO~1.11\bin;C:\PROGRA~2\WI3CF2~1\8.1\bin\x64;C:\tools\msys64\usr\bin"
