809d292beaa7f1284bb1969659bc54ac09caf38b
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2020-10-27 15:51:57 -0500

    only install devkit once

--- scripts/windows/installs/ruby_devkit.bat
+++
@@ -1,7 +0,0 @@
-choco install -y ruby2.devkit
-setx PATH "%PATH%;C:\tools\DevKit2\bin"
-cd C:\tools\DevKit2
-echo "- C:\tools\ruby26" >> config.yml
-C:\tools\ruby26\bin\ruby.exe dk.rb install --force
-C:\tools\ruby26\bin\ridk.cmd install 3
-refreshenv
