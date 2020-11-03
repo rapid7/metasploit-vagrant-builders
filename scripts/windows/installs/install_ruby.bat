a397a05849994fde46e372dba069b854ceac32b4
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2020-11-03 14:29:21 -0600

    only install devkit once - retry

--- scripts/windows/installs/install_ruby.bat
+++ scripts/windows/installs/install_ruby.bat
@@ -1,10 +1,2 @@
 choco install -y ruby --version 2.6.5.1
 refreshenv
-choco install -y ruby2.devkit
-setx PATH "%PATH%;C:\tools\DevKit2\bin"
-refreshenv
-cd C:\tools\DevKit2
-echo "- C:\tools\ruby26" >> config.yml
-ruby dk.rb install --force
-ridk install 3
-refreshenv
