5b6b80422112a91d607a96881cd2c018e771133a
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2020-11-04 11:35:37 -0600

    Revert "reorder path to put msys2 objects first"
    This reverts commit fc3e1f5dec014a81072153192f92cdbaedcf1927.

--- scripts/windows/configs/set_path.bat
+++ scripts/windows/configs/set_path.bat
@@ -1,1 +1,1 @@
-setx -m PATH "C:\tools\msys64\usr\bin;%PATH%;C:\PROGRA~2\WIXTOO~1.11\bin;C:\PROGRA~2\WI3CF2~1\8.1\bin\x64"
+setx -m PATH "%PATH%;C:\PROGRA~2\WIXTOO~1.11\bin;C:\PROGRA~2\WI3CF2~1\8.1\bin\x64;C:\tools\msys64\usr\bin"
