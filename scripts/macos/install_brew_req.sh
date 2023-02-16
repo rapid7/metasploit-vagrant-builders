82804db58afc2976788a7814779db5263247a2b0
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2023-02-16 12:02:10 -0600

    fixed brew install

--- scripts/macos/install_brew_req.sh
+++ scripts/macos/install_brew_req.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-su vagrant -c 'echo "\n" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+su vagrant -c 'echo "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
 
 su vagrant -c '/bin/bash -l -c "brew install coreutils m4 automake mingw-w64 gpg ldid"'
 
