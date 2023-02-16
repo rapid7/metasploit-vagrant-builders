9e06f6d2c964ef6855d2851885c715ec1bf32ab2
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2023-02-16 10:59:36 -0600

    adjust parallel

--- buildBoxes.py
+++ buildBoxes.py
@@ -69,7 +69,7 @@ def build_base(packer_var_file, root_path, replace_existing=False, vmServer=None
             return p  # just return without exec since ret value is not checked anyways
 
     try:
-        p.build(parallel=True, debug=False, force=False)
+        p.build(parallel=False, debug=False, force=False)
     except sh.ErrorReturnCode:
         print "Error: build of " + prependString + vm_name + " returned non-zero"
     os.chdir(abs_root)
