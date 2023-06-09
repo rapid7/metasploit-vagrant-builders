f5e4f60ff108a7f4b3dfc1fa4b7ebc022bc170ed
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2023-06-08 19:13:17 -0500

    allow override of vagrant key with via resources

--- resources/windows/windows.pkr.hcl
+++ resources/windows/windows.pkr.hcl
@@ -117,7 +117,7 @@ build {
   provisioner "powershell" {
     elevated_user     = var.install_user
     elevated_password = var.install_pass
-    inline            = ["[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:/Users/vagrant/.ssh/authorized_keys')"]
+    inline            = ["cp C:/vagrant/resources/authorized_keys C:/Users/vagrant/.ssh/authorized_keys"]
   }
 
   provisioner "powershell" {
--- scripts/windows/configs/vagrant-ssh.bat
+++ scripts/windows/configs/vagrant-ssh.bat
@@ -1,6 +1,6 @@
-:: vagrant public key
-if exist a:\vagrant.pub (
-  copy a:\vagrant.pub C:\Users\vagrant\.ssh\authorized_keys
+:: vagrant public key or locally provided override
+if exist C:\vagrant\resources\authorized_keys (
+  copy C:\vagrant\resources\authorized_keys C:\Users\vagrant\.ssh\authorized_keys
 ) else (
   powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:\Users\vagrant\.ssh\authorized_keys')" <NUL
 )
